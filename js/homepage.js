12f618deb58d54034520146ffa13755db63aa956
Joanne0119 <liuchengen52033@gmail.com>
2023-10-28 21:45:25 +0800

    add cards hover animation with js

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,19 @@
 const navbar = document.querySelector('nav');
+const cards = document.querySelectorAll('.card');
 
 addEventListener('scroll', () => {
   navbar.classList.toggle('sticky', window.scrollY > 0);
+})
+
+cards.forEach((card) => {
+  const playIcon = card.querySelector('i');
+  console.log(playIcon);
+  card.addEventListener('mouseenter', () => {
+    console.log('mouse Enter');
+    playIcon.classList.add('toPlay');
+  })
+  card.addEventListener('mouseleave', () => {
+    console.log('mouse Leave');
+    playIcon.classList.remove('toPlay');
+  })
 })
