b29fefbedb411b3c2e7abb7399a6c9d2a6a1b852
Joanne0119 <liuchengen52033@gmail.com>
2023-10-29 21:12:02 +0800

    add library hover effect and animation

---
+++ js/library.js
@@ -0,0 +1,18 @@
+const songs = document.querySelectorAll('.song');
+
+songs.forEach((song) => {
+  const deleteIcons = song.querySelectorAll('i');
+  
+  song.addEventListener('mouseenter', () => {
+    console.log('mouse Enter');
+    deleteIcons.forEach((deleteIcon) => {
+      deleteIcon.classList.add('toDelete');
+    });
+  })
+  song.addEventListener('mouseleave', () => {
+    console.log('mouse Leave');
+    deleteIcons.forEach((deleteIcon) => {
+      deleteIcon.classList.remove('toDelete');
+    });
+  })
+})
