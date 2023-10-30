44ea07c37ef74565d9f8fc7b21cbed5b037b60a8
dddCrazy87 <n126970@gmail.com>
2023-10-31 03:09:54 +0800

    Add search result highlight

--- js/search.js
+++ js/search.js
@@ -2,6 +2,21 @@ const searchInput = document.getElementById('search-input');
 const searchResults = document.getElementById('search-results');
 const resultsList = document.getElementById('results-list');
 
+function MarkSearchTerm(string, target) {
+
+    const fId = string.indexOf(target);
+    if (fId !== -1) {
+        return (string.substring(0, fId-1)
+                + "<span class=\"search-result-highlight\">"
+                + string.substring(fId, fId+target.length)
+                + "</span>"
+                + string.substring(fId+target.length));
+    }
+    else {
+        return (string);
+    }
+}
+
 searchInput.addEventListener('input', function () {
 
     const searchTerm = searchInput.value.toLowerCase();
@@ -15,20 +30,19 @@ searchInput.addEventListener('input', function () {
 
     resultsList.innerHTML = '';
     const filteredResults = songData.filter(item => item[0][0].toLowerCase().includes(searchTerm) || item[1][0].toLowerCase().includes(searchTerm));
-    console.log(filteredResults);
+
     if (filteredResults.length > 0 && filteredResults.length != songData.length) {
         searchResults.style.display = "block";
         filteredResults.forEach(result => {
-
             const htmlString = `
                 <div class="card border-0 p-3" >
-                    <img class="card-image rounded" src="images/${result[0]}.jpg" alt="${result[0]}">
+                    <img class="card-image rounded" src="images/${result[0][0]}.jpg" alt="${result[0][0]}">
                     <i class="fa-solid fa-plus"></i>
                     <i class="fa-solid fa-play"></i>
                     <div class="card-body">
-                        <h4 class="card-title text-light song-title">${result[0]}</h4>
-                        <p class="card-text text-light singer">${result[1]}</p>
-                    </div>           
+                        <h4 class="card-title text-light song-title">${MarkSearchTerm(result[0][0], searchTerm)}</h4>
+                        <p class="card-text text-light singer">${MarkSearchTerm(result[1][0], searchTerm)}</p>
+                    </div>
                 </div>
             `;
             resultsList.innerHTML += htmlString;
