7194026a6632ae9795ee622c2aff9dcec5332e3a
Joanne0119 <liuchengen52033@gmail.com>
2023-11-01 17:02:23 +0800

    add some samples to the songs.js

---
+++ js/songs.js
@@ -0,0 +1,27 @@
+const songs = [
+  {
+    id: "001",
+    image: "../images/披星戴月的想你.jpg",
+    title: "披星戴月的想你",
+    singer: "告五人",
+    added: false
+  },{
+    id: "002",
+    image: "../images/突然好想你.jpg",
+    title: "突然好想你",
+    singer: "五月天",
+    added: false
+  },{
+    id: "003",
+    image: "../images/唯一.jpg",
+    title: "唯一",
+    singer: "G.E.M. 鄧紫棋",
+    added: false
+  },{
+    id: "004",
+    image: "../images/唯一.jpg",
+    title: "閣愛你一擺",
+    singer: "茄子蛋",
+    added: false
+  }
+]
