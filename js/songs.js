6519763d7e023a8d75ad1318a15d9a240e588ad1
Joanne0119 <liuchengen52033@gmail.com>
2023-11-01 17:05:42 +0800

    edit songs.js image path

--- js/songs.js
+++ js/songs.js
@@ -1,25 +1,25 @@
-const songs = [
+export const songs = [
   {
     id: "001",
-    image: "../images/披星戴月的想你.jpg",
+    image: "images/披星戴月的想你.jpg",
     title: "披星戴月的想你",
     singer: "告五人",
     added: false
   },{
     id: "002",
-    image: "../images/突然好想你.jpg",
+    image: "images/突然好想你.jpg",
     title: "突然好想你",
     singer: "五月天",
     added: false
   },{
     id: "003",
-    image: "../images/唯一.jpg",
+    image: "images/唯一.jpg",
     title: "唯一",
     singer: "G.E.M. 鄧紫棋",
     added: false
   },{
     id: "004",
-    image: "../images/唯一.jpg",
+    image: "images/唯一.jpg",
     title: "閣愛你一擺",
     singer: "茄子蛋",
     added: false
