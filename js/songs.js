d67adde4a0cab8f6b81fa1c1675c81654cd11f42
Joanne0119 <liuchengen52033@gmail.com>
2023-11-02 10:43:59 +0800

    add more sample in songsLibrary array

--- js/songs.js
+++ js/songs.js
@@ -19,9 +19,39 @@ export const songsLibrary = [
     added: true
   },{
     id: "004",
-    image: "images/唯一.jpg",
+    image: "images/閣愛你一擺.jpg",
     title: "閣愛你一擺",
     singer: "茄子蛋",
+    added: true
+  },{
+    id: "005",
+    image: "images/好不容易.jpg",
+    title: "好不容易",
+    singer: "告五人",
+    added: true
+  },{
+    id: "006",
+    image: "images/without you.jpg",
+    title: "Without You",
+    singer: "高爾宣OSN",
+    added: true
+  },{
+    id: "007",
+    image: "images/cruel summer.jpg",
+    title: "Cruel Summer",
+    singer: "Taylor Swfit",
+    added: true
+  },{
+    id: "008",
+    image: "images/LMF.jpg",
+    title: "LMF",
+    singer: "POPO J",
+    added: true
+  },{
+    id: "009",
+    image: "images/one of your girls.jpg",
+    title: "One of Your Girls",
+    singer: "Troye Sivan",
     added: true
   }
 ]
