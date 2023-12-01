c8bf68db7b5a4f553e0b8b46868248d42d76e367
Joanne0119 <liuchengen52033@gmail.com>
2023-12-01 20:25:29 +0800

    add next and pre button to scroll the card section

--- js/cards.js
+++ js/cards.js
@@ -18,4 +18,40 @@ function AddEventToCard() {
   })
 }
 
+function cardScroll(){
+  const cardsSection = document.querySelectorAll('.cards-section');
+
+  cardsSection.forEach((cardSection) => {
+    const cards = cardSection.querySelector('.cards');
+    const card = cards.querySelectorAll('.card');
+    const leftBtn = cardSection.querySelector('.left-btn');
+    const rightBtn = cardSection.querySelector('.right-btn');
+    const cardsWidth = Number(cards.scrollWidth);
+    const cardsSectionWidth = Number(cardSection.offsetWidth);
+    const cardWidth = Number(card[0].offsetWidth);
+    console.log(cardsWidth);
+    console.log(cardsSectionWidth);
+    console.log(rightBtn);
+    if(cardsWidth >= (cardsSectionWidth + 5)) {
+      rightBtn.classList.remove('display-none');
+      leftBtn.classList.remove('display-none');
+    }
+    else {
+      leftBtn.classList.add('display-none');
+      rightBtn.classList.add('display-none');
+    }
+    let totAdded = 0
+    rightBtn.addEventListener('click', () => {
+      totAdded -= cardWidth;
+      cards.style.transform = `translateX(${totAdded}px)`;
+    });
+    leftBtn.addEventListener('click', () => {
+      totAdded += cardWidth;
+      cards.style.transform = `translateX(${totAdded}px)`;
+    })
+  });
+}
+
+cardScroll();
+window.onresize = cardScroll;
 AddEventToCard();
