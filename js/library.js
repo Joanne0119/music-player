51f56583c4dcb23215aece20e94587730bbc3f09
Joanne0119 <liuchengen52033@gmail.com>
2023-12-10 19:04:50 +0800

    renew library.js

--- js/library.js
+++ js/library.js
@@ -64,7 +64,6 @@ function deleteSong(delBtnId) {
   })
   console.log('delete!');
   localStorage.setItem('songsLibrary', JSON.stringify(songsLibrary));
-  console.log(localStorage.setItem('songsLibrary',JSON.stringify(songsLibrary)));
   console.log(songsLibrary);
 }
 
