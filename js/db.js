306543e8c7053efb7497246189a813c2e5e7cc8a
dddCrazy87 <n126970@gmail.com>
2023-12-14 21:01:52 +0800

    update db scripts

--- js/db.js
+++ js/db.js
@@ -1,14 +1,34 @@
-import { songsLibrary } from "./songs";
-
 let isLogin = true;
 export const songsList = [];
 export let currentPlaying;
+export let currentPlayingTime;
 
 if(isLogin) {
     songsList = [1,3,5,7];
     currentPlaying = 1;
+    currentPlayingTime = 120;
 }
 else {
     songsList = [];
     currentPlaying = 0;
+    currentPlayingTime = 0;
+}
+
+export function addToSongsList(id) {
+    songsList.push(id);
+    if(isLogin) {
+        // db
+    }
+}
+
+export function removeFromSongsList(id) {
+    songsList.forEach(song => {
+        if(song == id) {
+            songsList = songsList.filter(item => item !== song);
+            return;
+        }
+    });
+    if(isLogin) {
+        // db
+    }
 }
