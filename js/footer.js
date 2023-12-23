b483b13be9c8ba4dac5c9debc73df66633f28db0
Joanne0119 <liuchengen52033@gmail.com>
2023-12-23 22:16:27 +0800

    edit footer width and combine some detials

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+const footer = document.querySelector('footer');
+const playerSection = document.querySelector('.player-section');
+
+function differentPagesWidth(){
+  if(playerSection){
+    footer.classList.add('.widthCompatible');
+  }
+  else{
+    footer.classList.remove('.widthCompatible');
+  }
+}
